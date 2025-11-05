af0afa1c9292f0dd324af22efbe18321eec76b64
Christina Jernberg <cjernbe@wgu.edu>
2025-11-04 21:04:06 -0800

    Day 8 complete! The Caesar Cypher! This one was challenging, but it was still fun!

---
+++ Day 8/Functions with Inputs/task.py
@@ -0,0 +1,13 @@
+def greet():
+    print("Hello")
+    print("How do you do?")
+    print("Isn't the weather nice?")
+
+greet()
+
+def greet_with_input(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}?")
+
+name = "chuck"
+greet_with_input(name)
--- Day 8/Positional vs Keyword Arguments/task.py
+++ Day 8/Positional vs Keyword Arguments/task.py
@@ -1,8 +1,17 @@
 # Functions with input
 
-def greet_with_name(name):
-    print(f"Hello {name}")
-    print(f"How do you do {name}?")
+#def greet_with_name(name, location):
+#    print(f"Hello {name}")
+#    print(f"How do you do {name}?")
 
 
-greet_with_name("Jack Bauer")
+name_in = input(f"What is your name?: ")
+location_in = input(f"What is your location?: ")
+
+def greet_with_name(name, location):
+    print(f"Hello, {name_in}.")
+    print(f"What is it like in {location_in}?")
+
+#greet_with_name(name_in, location_in)
+
+greet_with_name(name=name_in, location=location_in)
