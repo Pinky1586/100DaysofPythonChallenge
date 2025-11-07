3e07e0c6c3d44335272b74bc6bb3000b26628d43
Christina Jernberg <cjernbe@wgu.edu>
2025-11-06 20:02:18 -0800

    Day 9 complete! Today's challenge was a blind auction project! I was actually able to jump right into this, get it nearly completely written, and then debug the majority of it in about 15 minutes.

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -1,6 +1,30 @@
 # TODO-1: Ask the user for input
-# TODO-2: Save data into dictionary {name: price}
-# TODO-3: Whether if new bids need to be added
-# TODO-4: Compare bids in dictionary
+import art
 
+print(art.logo)
+
+print("Welcome to the secret auction program.")
+players_bids = {
+
+}
+name = input("What is your name?: ")
+bid = input("What's your bid?: $ ")
+# TODO-2: Save data into dictionary {name: price}
+players_bids[name] = bid
+
+# TODO-3: Whether if new bids need to be added
+new_bidders = input("Are there any other bidders? Type 'y' or 'n': ")
+
+while new_bidders == 'y':
+    print("\n" * 100)
+    name = input("What is your name?: ")
+    bid = input("What's your bid?: ")
+    players_bids[name] = bid
+    new_bidders = input("Are there any other bidders? Type 'y' or 'n': ")
+
+# TODO-4: Compare bids in dictionary
+max_key = max(players_bids, key=players_bids.get)
+max_bid = max(players_bids.values())
+
+print(f"Congratulations {max_key}! You are the winning bidder with a bid of ${max_bid}!")
 
---
+++ Day 9/Nested Lists and Dictionaries/task.py
@@ -0,0 +1,24 @@
+# travel_log = {
+#     'France': ["Paris", "Lille", "Dijon"],
+#     'Germany': ["Stuttgart", "Berlin"],
+# }
+
+# list = ["Paris", "Lille", "Dijon"]
+# item = travel_log["France"][1]
+# print(item)
+
+# nested_list = ["A", "B", ["C", "D"]]
+# print(nested_list[2][1])
+
+travel_log = {
+    "France": {
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits": 12
+    },
+    "Germany": {
+        "cities_visited": ["Stuttgart", "Berlin"],
+        "total_visits": 5
+    },
+}
+
+print(travel_log["Germany"]["cities_visited"][0])
