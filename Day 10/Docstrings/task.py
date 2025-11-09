61d356349afe32d907a765ef6e050aa7443951e1
Christina Jernberg <cjernbe@wgu.edu>
2025-11-08 19:06:50 -0800

    The task for day 10 is to build a calculator! I got 90% of the logic built within 10 minutes. Took a little bit of time to get the loop functioning properly.

--- Day 10/Docstrings/task.py
+++ Day 10/Docstrings/task.py
@@ -1,4 +1,6 @@
 def format_name(f_name, l_name):
+    """Takes first and last name and formats it to return
+        title case version of the same"""
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
     return f"{formated_f_name} {formated_l_name}"
