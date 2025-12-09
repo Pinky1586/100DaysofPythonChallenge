80fdcdca451c238183bfeb95eed5f30d787c1fd8
Christina Jernberg <cjernbe@wgu.edu>
2025-12-09 09:39:45 -0800

    Day 14 will take me a few days. I'm creating a new game_data file.

---
+++ Day 14/Higher or Lower Project/game_data_pwhl.py
@@ -0,0 +1,302 @@
+data = [
+{
+'name': 'Sophie Shirley',
+'category': 'Player',
+'follower count': 5137,
+'team': 'Fleet'
+},
+{
+'name': 'Alina Müller',
+'category': 'Player',
+'follower count': 8561,
+'team': 'Fleet'
+},
+{
+'name': 'Liz Schepers',
+'category': 'Player',
+'follower count': 3705,
+'team': 'Fleet'
+},
+{
+'name': 'Olivia Mobley',
+'category': 'Player',
+'follower count': 1380,
+'team': 'Fleet'
+},
+{
+'name': 'Theresa Schafzahl',
+'category': 'Player',
+'follower count': 2465,
+'team': 'Fleet'
+},
+{
+'name': 'Abby Newhook',
+'category': 'Player',
+'follower count': 10300,
+'team': 'Fleet'
+},
+{
+'name': 'Hannah Brandt',
+'category': 'Player',
+'follower count': 15600,
+'team': 'Fleet'
+},
+{
+'name': 'Lara Kluge',
+'category': 'Player',
+'follower count': 3011,
+'team': 'Fleet'
+},
+{
+'name': 'Ella Huber',
+'category': 'Player',
+'follower count': 12000,
+'team': 'Fleet'
+},
+{
+'name': 'Shay Maloney',
+'category': 'Player',
+'follower count': 2283,
+'team': 'Fleet'
+},
+{
+'name': 'Loren Gabel',
+'category': 'Player',
+'follower count': 15100,
+'team': 'Fleet'
+},
+{
+'name': 'Jill Saulnier',
+'category': 'Player',
+'follower count': 20000,
+'team': 'Fleet'
+},
+{
+'name': 'Jamie Lee Rattray',
+'category': 'Player',
+'follower count': 15100,
+'team': 'Fleet'
+},
+{
+'name': 'Susanna Tapani',
+'category': 'Player',
+'follower count': 12600,
+'team': 'Fleet'
+},
+{
+'name': 'Boston Fleet',
+'category': 'Team',
+'follower count': 86300
+},
+{
+'name': 'Zoe Boyd',
+'category': 'Player',
+'follower count': 27400,
+'team': 'Fleet'
+},
+{
+'name': 'Megan Keller',
+'category': 'Player',
+'follower count': 26400,
+'team': 'Fleet',
+},
+{
+'name': 'Hadley Hartmetz',
+'category': 'Player',
+'follower count': 3900,
+'team': 'Fleet',
+},
+{
+'name': 'Mia Biotti',
+'category': 'Player',
+'follower count': 3300,
+'team': 'Fleet',
+},
+{
+'name': 'Haley Winn',
+'category': 'Player',
+'follower count': 6600,
+'team': 'Fleet',
+},
+{
+'name': 'Riley Brengman',
+'category': 'Player',
+'follower count': 2237,
+'team': 'Fleet',
+},
+{
+'name': 'Rylind MacKinnon',
+'category': 'Player',
+'follower count': 1900,
+'team': 'Fleet',
+},
+{
+'name': 'Daniela Pejšová',
+'category': 'Player',
+'follower count': 13600,
+'team': 'Fleet',
+},
+{
+'name': 'Olivia Zafuto',
+'category': 'Player',
+'follower count': 4100,
+'team': 'Fleet',
+},
+{
+'name': 'Katy Knoll',
+'category': 'Player',
+'follower count': 2600,
+'team': 'Frost',
+},
+{
+'name': 'Claire Butorac',
+'category': 'Player',
+'follower count': 2900,
+'team': 'Frost',
+},
+{
+'name': 'Kelly Pannek',
+'category': 'Player',
+'follower count': 7100,
+'team': 'Frost',
+},
+{
+'name': 'Grace Zumwinkle',
+'category': 'Player',
+'follower count': 7900,
+'team': 'Frost',
+},
+{
+'name': 'Dominique Petrie',
+'category': 'Player',
+'follower count': 2900,
+'team': 'Frost',
+},
+{
+'name': "Kaitlyn O'Donohoe",
+'category': 'Player',
+'follower count': 1300,
+'team': 'Frost',
+},
+{
+'name': 'Élizabeth Giguère',
+'category': 'Player',
+'follower count': 3900,
+'team': 'Frost',
+},
+{
+'name': 'Vanessa Upson',
+'category': 'Player',
+'follower count': 1500,
+'team': 'Frost',
+},
+{
+'name': 'Kendall Coyne Schofield',
+'category': 'Player',
+'follower count': 62900,
+'team': 'Frost',
+},
+{
+'name': 'Taylor Heise',
+'category': 'Player',
+'follower count': 6100,
+'team': 'Frost',
+},
+{
+'name': 'Denisa Křížová',
+'category': 'Player',
+'follower count': 1100,
+'team': 'Frost',
+},
+{
+'name': 'Klára Hymlárová',
+'category': 'Player',
+'follower count': 3500,
+'team': 'Frost',
+},
+{
+'name': 'Abby Hustler',
+'category': 'Player',
+'follower count': 2200,
+'team': 'Frost',
+},
+{
+'name': 'Britta Curle-Salemme',
+'category': 'Player',
+'follower count': 6200,
+'team': 'Frost',
+},
+{
+'name': 'Peyton Anderson',
+'category': 'Player',
+'follower count': 1300,
+'team': 'Frost',
+},
+{
+'name': 'Lee Stecklein',
+'category': 'Player',
+'follower count': 8300,
+'team': 'Frost',
+},
+{
+'name': 'Kendall Cooper',
+'category': 'Player',
+'follower count': 2500,
+'team': 'Frost',
+},
+{
+'name': 'Sidney Morin',
+'category': 'Player',
+'follower count': 3700,
+'team': 'Frost',
+},
+{
+'name': 'Madison Bizal',
+'category': 'Player',
+'follower count': 2000,
+'team': 'Frost',
+},
+{
+'name': 'Brooke Becker',
+'category': 'Player',
+'follower count': 3100,
+'team': 'Frost',
+},
+{
+'name': 'Ava Rinker',
+'category': 'Player',
+'follower count': 1100,
+'team': 'Frost',
+},
+{
+'name': 'Mae Batherson',
+'category': 'Player',
+'follower count': 6800,
+'team': 'Frost',
+},
+{
+'name': 'Natalie Buchbinder',
+'category': 'Player',
+'follower count': 3500,
+'team': 'Frost',
+},
+{
+'name': 'Marlène Boissonnault',
+'category': 'Player',
+'follower count': 15900,
+'team': 'Frost',
+},
+{
+'name': 'Nicole Hensley',
+'category': 'Player',
+'follower count': 16700,
+'team': 'Frost',
+},
+{
+'name': 'Maddie Rooney',
+'category': 'Player',
+'follower count': 26000,
+'team': 'Frost',
+},
+
+]
---
+++ Day 14/Higher or Lower Project/main.py
@@ -0,0 +1,4 @@
+#This project is going to take me several days because I wan't to make some basic changes to the game to turn it into
+#"Who of the PWHL Is More Instagram Famous?" It's the same basic idea and will function the same, but requires me to
+#build out a whole new gama_data dictionary. There will likely be several updates with little code while I work
+#on this project.
