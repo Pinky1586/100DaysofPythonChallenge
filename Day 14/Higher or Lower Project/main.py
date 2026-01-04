bfd845d2a8cad539bd39dd0d161c4f35978d76b8
Christina Jernberg <cjernbe@wgu.edu>
2026-01-04 14:26:36 -0800

    Day 14 update number 2. I've begun working on the logic of the game, but haven't gotten terribly far yet.

--- Day 14/Higher or Lower Project/main.py
+++ Day 14/Higher or Lower Project/main.py
@@ -1,4 +1,19 @@
-#This project is going to take me several days because I wan't to make some basic changes to the game to turn it into
-#"Who of the PWHL Is More Instagram Famous?" It's the same basic idea and will function the same, but requires me to
-#build out a whole new gama_data dictionary. There will likely be several updates with little code while I work
-#on this project.
+#Update number 2. I've begun working on some of the logic. I haven't yet completed the player's database.
+
+from art import logo
+from art import vs
+import random
+from game_data_pwhl import players
+
+print(r'Get ready to play:')
+print(logo)
+
+player1 = random.choice(players)
+player2 = random.choice(players)
+
+print(f'{player1["name"]} plays {player1["position"]} for the {player1["team"]}')
+print(vs)
+print(f'{player2["name"]} plays {player2["position"]} for the {player2["team"]}')
+
+print(f'Who has more Instagram followers A {player1["name"]} or B {player2["name"]}?: ')
+
